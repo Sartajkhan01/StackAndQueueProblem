f76673e43ad774eb73ae406fdd83bd8221011fc1
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-06 22:24:48 +0530

    Stack And Queue Program added

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack and Queue program \n---------------------------------------");
+            Stack stack = new Stack();
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+
+            stack.Push(node1);
+            stack.Push(node2);
+            stack.Push(node3);
+            stack.DisplayStack();
+
+        }
+    }
+}
